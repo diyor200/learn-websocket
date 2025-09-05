af66f46a9c8ef93cd31efd6aabafd54b6d39a889
macbook <abdulaxatovdiyorbek40@gmail.com>
2025-09-05 07:46:14 +0500

    file watch writer

--- main.go
+++ main.go
@@ -1,170 +1,101 @@
-// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
-	"flag"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/gorilla/websocket"
 )
 
 const (
-	// Time allowed to write the file to the client.
-	writeWait = 10 * time.Second
-
-	// Time allowed to read the next pong message from the client.
-	pongWait = 60 * time.Second
-
-	// Send pings to client with this period. Must be less than pongWait.
+	writeWait  = 10 * time.Second
+	pongWait   = 60 * time.Second
 	pingPeriod = (pongWait * 9) / 10
-
-	// Poll file for changes with this period.
 	filePeriod = 10 * time.Second
 )
 
 var (
-	addr      = flag.String("addr", ":8080", "http service address")
 	homeTempl = template.Must(template.New("").Parse(homeHTML))
 	filename  string
 	upgrader  = websocket.Upgrader{
-		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
+		ReadBufferSize:  1024,
 	}
 )
 
-func readFileIfModified(lastMod time.Time) ([]byte, time.Time, error) {
+func readFileIfMpdified(lastMod time.Time) ([]byte, time.Time, error) {
 	fi, err := os.Stat(filename)
 	if err != nil {
+		log.Println(err)
 		return nil, lastMod, err
 	}
+
 	if !fi.ModTime().After(lastMod) {
 		return nil, lastMod, nil
 	}
 	p, err := os.ReadFile(filename)
 	if err != nil {
-		return nil, fi.ModTime(), err
+		log.Println(err)
+		return nil, lastMod, err
 	}
+
 	return p, fi.ModTime(), nil
 }
 
-func reader(ws *websocket.Conn) {
-	defer ws.Close()
-	ws.SetReadLimit(512)
-	ws.SetReadDeadline(time.Now().Add(pongWait))
-	ws.SetPongHandler(func(string) error { ws.SetReadDeadline(time.Now().Add(pongWait)); return nil })
+func reader(conn *websocket.Conn) {
+	conn.SetReadLimit(512)
+	conn.SetReadDeadline(time.Now().Add(pongWait))
+	conn.SetPongHandler(func(string) error { conn.SetReadDeadline(time.Now().Add(pongWait)); return nil })
 	for {
-		_, _, err := ws.ReadMessage()
+		_, _, err := conn.ReadMessage()
 		if err != nil {
+			log.Println(err)
 			break
 		}
 	}
+	conn.Close()
 }
 
-func writer(ws *websocket.Conn, lastMod time.Time) {
+func writer(conn *websocket.Conn, lastmod time.Time) {
 	lastError := ""
 	pingTicker := time.NewTicker(pingPeriod)
 	fileTicker := time.NewTicker(filePeriod)
+
 	defer func() {
-		pingTicker.Stop()
 		fileTicker.Stop()
-		ws.Close()
+		pingTicker.Stop()
+		conn.Close()
 	}()
+
 	for {
 		select {
 		case <-fileTicker.C:
 			var p []byte
 			var err error
 
-			p, lastMod, err = readFileIfModified(lastMod)
-
+			p, lastmod, err = readFileIfMpdified(lastmod)
 			if err != nil {
-				if s := err.Error(); s != lastError {
-					lastError = s
-					p = []byte(lastError)
-				}
-			} else {
-				lastError = ""
-			}
-
-			if p != nil {
-				ws.SetWriteDeadline(time.Now().Add(writeWait))
-				if err := ws.WriteMessage(websocket.TextMessage, p); err != nil {
-					return
-				}
-			}
-		case <-pingTicker.C:
-			ws.SetWriteDeadline(time.Now().Add(writeWait))
-			if err := ws.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Println(err)
 				return
 			}
 		}
 	}
 }
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
+func home(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			log.Println(err)
-		}
+		log.Println(err)
 		return
 	}
-
-	var lastMod time.Time
-	if n, err := strconv.ParseInt(r.FormValue("lastMod"), 16, 64); err == nil {
-		lastMod = time.Unix(0, n)
-	}
-
-	go writer(ws, lastMod)
-	reader(ws)
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", http.StatusNotFound)
-		return
-	}
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	p, lastMod, err := readFileIfModified(time.Time{})
-	if err != nil {
-		p = []byte(err.Error())
-		lastMod = time.Unix(0, 0)
-	}
-	var v = struct {
-		Host    string
-		Data    string
-		LastMod string
-	}{
-		r.Host,
-		string(p),
-		strconv.FormatInt(lastMod.UnixNano(), 16),
-	}
-	homeTempl.Execute(w, &v)
+	defer conn.Close()
+	reader(conn)
 }
 
 func main() {
-	flag.Parse()
-	if flag.NArg() != 1 {
-		log.Fatal("filename not specified")
-	}
-	filename = flag.Args()[0]
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", serveWs)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
-		log.Fatal(err)
-	}
 }
 
 const homeHTML = `<!DOCTYPE html>
